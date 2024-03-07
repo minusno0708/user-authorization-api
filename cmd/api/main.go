4d164b3a1ad044cec2a7a44def34a3f4e8df6e70
minusno0708 <naoyao1014@gmail.com>
2024-03-08 00:49:22 +0900

    fix:エンドポイントのクエリ部分を削除

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,19 +10,20 @@ import (
 
 func main() {
 	userPersistence := persistence.NewUserPersistence()
-	userUseCase := usecase.NewUserUseCase(userPersistence)
-	userHandler := handler.NewUserHandler(userUseCase)
-
 	tokenPersistence := persistence.NewTokenPersistence()
+
+	userUseCase := usecase.NewUserUseCase(userPersistence)
 	tokenUseCase := usecase.NewTokenUseCase(tokenPersistence)
+
+	userHandler := handler.NewUserHandler(userUseCase, tokenUseCase)
 	authHandler := handler.NewAuthHandler(userUseCase, tokenUseCase)
 
 	r := gin.Default()
 	r.GET("/", userHandler.HandleConnectionAPI)
 	r.POST("/signup", userHandler.HandleUserSignup)
-	r.GET("/user/:user_id", userHandler.HandleUserGet)
-	r.PUT("/user/:user_id", userHandler.HandleUserPut)
-	r.DELETE("/user/:user_id", userHandler.HandleUserDelete)
+	r.GET("/user", userHandler.HandleUserGet)
+	r.PUT("/user", userHandler.HandleUserPut)
+	r.DELETE("/user", userHandler.HandleUserDelete)
 
 	r.POST("/signin", authHandler.HandleSignin)
 	r.DELETE("/signout", authHandler.HandleSignout)
