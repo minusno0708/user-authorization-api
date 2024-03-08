bec6eefd488d71dbe554938217b1c84cea39b8ed
minusno0708 <naoyao1014@gmail.com>
2024-03-08 11:51:13 +0900

    test:トークンドメインのテストを作成

---
+++ domain/token_test.go
@@ -0,0 +1,26 @@
+package domain
+
+import "testing"
+
+func TestDomainToken(t *testing.T) {
+	userID := "test_user"
+	token := NewToken(userID)
+	tokenString, err := token.ToString()
+	if err != nil {
+		t.Error("Error while generating token")
+	}
+	if tokenString == "" {
+		t.Error("TokenString is empty")
+	}
+
+	parsedToken, err := ParseToken(tokenString)
+	if err != nil {
+		t.Error("Error while parsing token")
+	}
+	if parsedToken.UserID() != userID {
+		t.Error("UserID is not matched")
+	}
+	if parsedToken.UUID() == "" {
+		t.Error("UUID is empty")
+	}
+}
